05e4524a92c58e6f9e500061de8fe387ad2fafe4
koba1108 <paseli.koba@gmail.com>
2022-07-06 20:06:50 +0900

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
   target: "static",
   app: {
     cdnURL: 'https://koba1108.github.io/nuxt3-agora-ts/'
